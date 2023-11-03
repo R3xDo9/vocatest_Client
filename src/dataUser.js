d54510d9b778e9a1871912f390c1519f074630ce
Oscar felipe Arias Alfonso <oscarfelipeariasalfonso@gmail.com>
2023-11-02 22:23:57 -0500

    bruh

--- src/dataUser.js
+++ src/dataUser.js
@@ -1,14 +1,14 @@
 export const dataUser=[
     {
-        id:1, imgUrl:"src/assets/user1.jpg", nombre:"Danna Valeria Romero Rintá." , data:''
+        id:1, imgUrl:"src/user1.jpg", nombre:"Danna Valeria Romero Rintá." , data:''
     },
     {
-        id:2, imgUrl:"src/assets/user2.jpg", nombre:"Angie Viviana Alfonso Medellín" , data:''
+        id:2, imgUrl:"src/user2.jpg", nombre:"Angie Viviana Alfonso Medellín" , data:''
     },
     {
-        id:3, imgUrl:"src/assets/user3.jpg", nombre:"Valery Dayanna Barreto Buitrago" , data:''
+        id:3, imgUrl:"src/user3.jpg", nombre:"Valery Dayanna Barreto Buitrago" , data:''
     },
     {
-        id:4, imgUrl:"src/assets/user4.jpg", nombre:"Alix Estefanía Maldonado Roa" , data:''
+        id:4, imgUrl:"src/user4.jpg", nombre:"Alix Estefanía Maldonado Roa" , data:''
     }
 ]
